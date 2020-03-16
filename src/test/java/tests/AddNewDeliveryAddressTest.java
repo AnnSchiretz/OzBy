bcfb8e2ab0f814827fc44e4be99c36c92ce3985c
Hanna Szczyriec <sisyrinchium02@gmail.com>
2020-03-16 11:40:45 +0300

    rename test file for maven

--- src/test/java/tests/AddNewDeliveryAddressTest.java
+++ src/test/java/tests/AddNewDeliveryAddressTest.java
@@ -3,7 +3,7 @@ package tests;
 import models.Address;
 import org.testng.annotations.Test;
 
-public class AddNewDeliveryAddress extends BaseTest {
+public class AddNewDeliveryAddressTest extends BaseTest {
     @Test
     public void addNewAddress() {
         Address address = new Address("220083", "Некрасова", "43", "5", "6", "4");
