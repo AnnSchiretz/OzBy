99506de3890b17863f927c6c7da2fcc31999bb05
Hanna Szczyriec <sisyrinchium02@gmail.com>
2020-03-18 20:30:24 +0300

    add TestListener add log4j2.yaml

--- src/test/java/tests/base/BaseTest.java
+++ src/test/java/tests/base/BaseTest.java
@@ -1,9 +1,10 @@
-package tests;
+package tests.base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import steps.LogInSteps;
 import steps.PersonalInfoSteps;
 import steps.ProductSteps;
@@ -11,13 +12,16 @@ import steps.UserSteps;
 import utils.CapabilitiesGenerator;
 import utils.PropertyManager;
 
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+@Listeners(TestListener.class)
 public class BaseTest {
     private WebDriver driver;
-    LogInSteps login;
-    PersonalInfoSteps infoSteps;
-    UserSteps userSteps;
-    PropertyManager props;
-    ProductSteps product;
+    public LogInSteps login;
+    public PersonalInfoSteps infoSteps;
+    public UserSteps userSteps;
+    public PropertyManager props;
+    public ProductSteps product;
 
     @BeforeMethod(description = "Opening Chrome Driver")
     public void createDriver() {
