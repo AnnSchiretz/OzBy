bc16e8760704d31cf7774597862805c01fe18c8a
AnnSchiretz <sisyrinchium02@gmail.com>
2020-03-20 20:23:07 +0300

    resolve conflicts(3)

--- src/test/java/page/BasketPage.java
+++ src/test/java/page/BasketPage.java
@@ -13,9 +13,9 @@ import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotEquals;
 
 public class BasketPage extends BasePage {
-    private static final By PRODUCT_IN_BASKET = By.id("goods-block");
+    private static final By PRODUCT_IN_BASKET = By.cssSelector(".i-layout-column_full table");
     private static final By EMPTY_BASKET = By.cssSelector(".i-textual__plain");
-    private static final By PRODUCT_LIST_IN_BASKET = By.xpath("//table[@id='goods-block']//tr[contains(@class,'goods-table__row')]");
+    private static final By PRODUCT_LIST_IN_BASKET = By.xpath("//div[contains(@class, 'i-layout-column_full')]//tr[contains(@class, 'goods-table__row ')]");
     private static final By PRODUCT_NAME = By.xpath("//div[@class='goods-table-cell__link']/a");
     private static final By PRODUCT_CHECKBOX = By.xpath("//div[@class='goods-table-cell']//input");
     private static final By DELETE_BUTTON = By.xpath("//div[contains(@class,'i-layout-column_full')]//button[contains(@class, 'remove')]");
@@ -35,7 +35,7 @@ public class BasketPage extends BasePage {
     }
 
     private void isPageOpened() {
-        if (driver.findElements(INFO_MODAL_LIST).size() > 1) {
+        if (driver.findElements(INFO_MODAL_LIST).size() > 2) {
             driver.findElement(POP_UP_INFO).click();
         }
         if (driver.findElements(PRODUCT_IN_BASKET).isEmpty()) {
@@ -74,13 +74,13 @@ public class BasketPage extends BasePage {
             String productName = product.findElement(PRODUCT_NAME).getText();
             if (productName.equals(name)) {
                 product.findElement(PRODUCT_CHECKBOX).click();
-                if (driver.findElements(INFO_MODAL_LIST).size() > 1) {
+                if (driver.findElements(INFO_MODAL_LIST).size() > 2) {
                     AllureUtils.takeScreenshot(driver);
                     driver.findElement(POP_UP_INFO2).click();
                 }
                 driver.findElement(DELETE_BUTTON).click();
-                AllureUtils.takeScreenshot(driver);
-                driver.findElement(DELETE_CONFIRMATION).click();
+                WebElement deleteButton = driver.findElement(DELETE_CONFIRMATION);
+                wait.until(ExpectedConditions.elementToBeClickable(deleteButton)).click();
                 break;
             }
         }
