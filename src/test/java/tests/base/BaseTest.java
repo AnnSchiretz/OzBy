152496d3506ecfb3aacf7c09fa769a33217af80f
AnnSchiretz <sisyrinchium02@gmail.com>
2020-03-20 20:20:17 +0300

    resolve conflicts(2)

--- src/test/java/tests/base/BaseTest.java
+++ src/test/java/tests/base/BaseTest.java
@@ -5,10 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
-import steps.LogInSteps;
-import steps.PersonalInfoSteps;
-import steps.ProductSteps;
-import steps.UserSteps;
+import steps.*;
 import utils.CapabilitiesGenerator;
 import utils.PropertyManager;
 
